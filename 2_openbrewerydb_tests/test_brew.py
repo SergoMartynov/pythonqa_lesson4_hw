2d68608718789f75bb15bcbbd40eedc269b8210e
SergoMartynov <sergo.martynov@gmail.com>
2020-08-04 16:18:10 +0300

    resolve some problems with checks in tests

--- 2_openbrewerydb_tests/test_brew.py
+++ 2_openbrewerydb_tests/test_brew.py
@@ -13,14 +13,16 @@ def test_404(base_url):
     assert r.status_code == 404
 
 
-def test_validation_id0(base_url, schema_id0):
-    r = response = requests.get(base_url + '/0')
-    validate({'message': "Couldn't find Brewery with 'id'=0"}, schema=schema_id0)
+def test_validation_id0(base_url):
+    response = requests.get(base_url + '/0')
+    jsonResponse = response.json()
+    validate({'message': "Couldn't find Brewery with 'id'=0"}, schema=jsonResponse)
 
 
-def test_validation_id1(base_url, schema_id1):
-    r = requests.get(base_url + '/1')
-    validate({'id': 1}, schema=schema_id1)
+def test_validation_id1(base_url):
+    response = requests.get(base_url + '/1')
+    jsonResponse = response.json()
+    validate({'id': 1}, schema=jsonResponse)
 
 
 @pytest.mark.parametrize("city_test", ['san_diego', 'amsterdam', 'new_york'])
